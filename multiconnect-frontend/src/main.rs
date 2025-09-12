4d05085839d46a607a3ca443cac83ffa9c14727c
totaltaxamount <shieldscoen@gmail.com>
2025-09-12 17:46:59 -0600

    misc: cleanup

--- multiconnect-frontend/src/main.rs
+++ multiconnect-frontend/src/main.rs
@@ -11,24 +11,6 @@ use multiconnect_lib::FrontendArgs;
 async fn main() -> Result<(), Box<dyn Error>> {
   let args: FrontendArgs = argh::from_env();
 
-  // let colors = ColoredLevelConfig::new().error(Color::Red).warn(Color::Yellow).info(Color::Green).debug(Color::Blue);
-
-  // fern::Dispatch::new()
-  //   .format(move |out, message, record| {
-  //     let bold_start = "\u{001b}[1m"; // Bold
-  //     let reset = "\u{001b}[0m"; // Reset
-
-  //     out.finish(format_args!(
-  //       "{} [{}] [{bold_start}{}{reset}] > {}",
-  //       humantime::format_rfc3339(SystemTime::now()),
-  //       colors.color(record.level()),
-  //       record.target(),
-  //       message
-  //     ))
-  //   })
-  //   .level(log::LevelFilter::from_str(&args.log_level)?)
-  //   .chain(std::io::stdout())
-  //   .apply()?;
   multiconnect_core::init_tracing(&args.log_level, args.port + 2)?; // Daemon on +1
 
   ConfigManager::init().await;
